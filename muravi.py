3c009a37281475bf90db349381da398b39cfb008
nevergona1 <148555828+nevergona1@users.noreply.github.com>
2023-11-15 14:38:30 +0500

    Update muravi.py

--- muravi.py
+++ muravi.py
@@ -1,128 +1,52 @@
-import os 
-import sys 
-import random 
-import pygame
+import tkinter as tk
 
-class Player(object): 
-     
-    def __init__(self): 
-        self.rect = pygame.Rect(32, 32, 16, 16) 
-  
-    def move(self, dx, dy): 
-         
-        # Move each axis separately. Note that this checks for collisions both times. 
-        if dx != 0: 
-            self.move_single_axis(dx, 0) 
-        if dy != 0: 
-            self.move_single_axis(0, dy) 
-     
-    def move_single_axis(self, dx, dy): 
-         
-        # Move the rect 
-        self.rect.x += dx 
-        self.rect.y += dy 
-  
-        # If you collide with a wall, move out based on velocity 
-        for wall in walls: 
-            if self.rect.colliderect(wall.rect): 
-                if dx > 0: # Moving right; Hit the left side of the wall 
-                    self.rect.right = wall.rect.left 
-                if dx < 0: # Moving left; Hit the right side of the wall 
-                    self.rect.left = wall.rect.right 
-                if dy > 0: # Moving down; Hit the top side of the wall 
-                    self.rect.bottom = wall.rect.top 
-                if dy < 0: # Moving up; Hit the bottom side of the wall 
-                    self.rect.top = wall.rect.bottom 
-  
-# Nice class to hold a wall rect 
-class Wall(object): 
-     
-    def __init__(self, pos): 
-        walls.append(self) 
-        self.rect = pygame.Rect(pos[0], pos[1], 16, 16) 
+def draw_maze(canvas, maze):
+    cell_size = 30  # Размер ячейки лабиринта
 
-# Initialise pygame 
-os.environ["SDL_VIDEO_CENTERED"] = "1" 
-pygame.init() 
-  
-# Set up the display 
-pygame.display.set_caption("Get to the red square!") 
-screen = pygame.display.set_mode((320, 240)) 
-  
-clock = pygame.time.Clock() 
-walls = [] # List to hold the walls 
-player = Player() # Create the player 
+    for row in range(len(maze)):
+        for col in range(len(maze[row])):
+            if maze[row][col] == 1:
+                cell_color = "black"
+            elif maze[row][col] == 2:
+                cell_color = "white"
+            else:
+                cell_color = "gray"
 
-def create_maze():
-    global walls, end_rect
-    walls = []  # Clear previous walls
-    player.rect.topleft = (32, 32)  # Reset player position
-    
-    # Define your level layout in a list of strings. W = wall, E = exit 
-    level = [ 
-        "WWWWWWWWWWWWWWWWWWWW", 
-        "W                  W", 
-        "W         WWWWWW   W", 
-        "W   WWWW       W   W", 
-        "W   W        WWWW  W", 
-        "W WWW  WWWW        W", 
-        "W   W     W W      W", 
-        "W   W     W   WWW WW", 
-        "W   WWW WWW   W W  W", 
-        "W     W   W   W W  W", 
-        "WWW   W   WWWWW W  W", 
-        "W W      WW        W", 
-        "W W   WWWW   WWW   W", 
-        "W     W    E   W   W", 
-        "WWWWWWWWWWWWWWWWWWWW", 
-    ] 
-    
-    x = y = 0 
-    for row in level: 
-        for col in row: 
-            if col == "W": 
-                Wall((x, y)) 
-            if col == "E": 
-                end_rect = pygame.Rect(x, y, 16, 16) 
-            x += 16 
-        y += 16 
-        x = 0 
+            x = col * cell_size
+            y = row * cell_size
+            canvas.create_rectangle(x, y, x + cell_size, y + cell_size, fill=cell_color, outline="black")
 
-create_maze()
+def toggle_cell(event):
+    col = event.x // cell_size
+    row = event.y // cell_size
 
-running = True 
-while running: 
-    clock.tick(60) 
-     
-    for e in pygame.event.get(): 
-        if e.type == pygame.QUIT: 
-            running = False 
-        if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE: 
-            running = False 
-  
-    # Move the player if an arrow key is pressed 
-    key = pygame.key.get_pressed() 
-    if key[pygame.K_LEFT]: 
-        player.move(-2, 0) 
-    if key[pygame.K_RIGHT]: 
-        player.move(2, 0) 
-    if key[pygame.K_UP]: 
-        player.move(0, -2) 
-    if key[pygame.K_DOWN]: 
-        player.move(0, 2) 
-  
-    # Just added this to make it slightly fun ;) 
-    if player.rect.colliderect(end_rect): 
-        create_maze() 
-  
-    # Draw the scene 
-    screen.fill((0, 0, 0)) 
-    for wall in walls: 
-        pygame.draw.rect(screen, (255, 255, 255), wall.rect) 
-    pygame.draw.rect(screen, (255, 0, 0), end_rect) 
-    pygame.draw.rect(screen, (255, 200, 0), player.rect) 
-    pygame.display.flip() 
-    clock.tick(360)
+    if 0 <= row < len(maze) and 0 <= col < len(maze[0]):
+        maze[row][col] = 1 if maze[row][col] == 0 else 0
+        draw_maze(canvas, maze)
 
-pygame.quit()
-sys.exit()
+# Создаем главное окно
+root = tk.Tk()
+root.title("Редактор лабиринта")
+
+# Создаем холст
+canvas_width = 600
+canvas_height = 600
+canvas = tk.Canvas(root, width=canvas_width, height=canvas_height, bg="white")
+canvas.pack()
+
+# Размер ячейки лабиринта
+cell_size = 30  
+
+# Создаем пустой лабиринт
+maze_width = canvas_width // cell_size
+maze_height = canvas_height // cell_size
+maze = [[0] * maze_width for _ in range(maze_height)]
+
+# Привязываем событие клика мыши
+canvas.bind("<Button-1>", toggle_cell)
+
+# Рисуем лабиринт на холсте
+draw_maze(canvas, maze)
+
+# Запускаем главный цикл
+root.mainloop()
